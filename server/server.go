b4803a009ae00e9bb39b5c86476d1981b2767e64
xiexiaojun <xie1xiao1jun@qq.com>
2019-07-03 23:05:12 +0800

    new

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/gookit/color"
 	"github.com/jander/golog/logger"
 	"github.com/kardianos/service"
 )
@@ -26,7 +27,7 @@ func On(n, dn, d string) *Service {
 
 func (sv *Service) Start(callBack func()) {
 	if len(sv.name) == 0 {
-		fmt.Printf("Service init faild,must first call On.\n")
+		color.Error.Render("Service init faild,must first call On.\n")
 		return
 	}
 	//name, displayName, desc := config.GetServiceConfig()
@@ -39,11 +40,11 @@ func (sv *Service) Start(callBack func()) {
 	s, err := service.New(p, sc)
 	//var s, err = service.NewService(name, displayName, desc)
 	if err != nil {
-		fmt.Printf("%s unable to start: %s", sv.displayName, err)
+		color.Error.Render("%s unable to start: %s", sv.displayName, err)
 		return
 	}
 
-	fmt.Printf("Service \"%s\" do.\n", sv.displayName)
+	color.Info.Render("Service \"%s\" do.\n", sv.displayName)
 
 	if len(os.Args) > 1 {
 		var err error
@@ -53,37 +54,37 @@ func (sv *Service) Start(callBack func()) {
 			{
 				err = s.Install()
 				if err != nil {
-					fmt.Printf("Failed to install: %s\n", err)
+					color.Error.Render("Failed to install: %s\n", err)
 					return
 				}
-				fmt.Printf("Service \"%s\" installed.\n", sv.displayName)
+				color.Info.Render("Service \"%s\" installed.\n", sv.displayName)
 			}
 		case "remove":
 			{
 				err = s.Uninstall()
 				if err != nil {
-					fmt.Printf("Failed to remove: %s\n", err)
+					color.Error.Render("Failed to remove: %s\n", err)
 					return
 				}
-				fmt.Printf("Service \"%s\" removed.\n", sv.displayName)
+				color.Info.Render("Service \"%s\" removed.\n", sv.displayName)
 			}
 		case "run":
 			{
 				err = s.Run()
 				if err != nil {
-					fmt.Printf("Failed to run: %s\n", err)
+					color.Error.Render("Failed to run: %s\n", err)
 					return
 				}
-				fmt.Printf("Service \"%s\" run.\n", sv.displayName)
+				color.Info.Render("Service \"%s\" run.\n", sv.displayName)
 			}
 		case "start":
 			{
 				err = s.Start()
 				if err != nil {
-					fmt.Printf("Failed to start: %s\n", err)
+					color.Error.Render("Failed to start: %s\n", err)
 					return
 				}
-				fmt.Println("starting check service:", sv.displayName)
+				color.Info.Render("starting check service:", sv.displayName)
 
 				ticker := time.NewTicker(1 * time.Second)
 				<-ticker.C
@@ -91,22 +92,22 @@ func (sv *Service) Start(callBack func()) {
 				var s ServiceTools
 				st, err := s.IsStart(sv.name)
 				if err != nil {
-					fmt.Println(err)
+					color.Error.Render(err)
 					return
 				} else {
 					if st == Stopped || st == StopPending {
-						fmt.Printf("Service \"%s\" is Stopped.\n", sv.displayName)
-						fmt.Println("can't to start service.")
+						color.Error.Render("Service \"%s\" is Stopped.\n", sv.displayName)
+						color.Error.Render("can't to start service.")
 						return
 					}
 				}
-				fmt.Printf("Service \"%s\" started.\n", sv.displayName)
+				color.Info.Render("Service \"%s\" started.\n", sv.displayName)
 			}
 		case "stop":
 			{
 				err = s.Stop()
 				if err != nil {
-					fmt.Printf("Failed to stop: %s\n", err)
+					color.Error.Render("Failed to stop: %s\n", err)
 					return
 				}
 				var s ServiceTools
@@ -116,18 +117,18 @@ func (sv *Service) Start(callBack func()) {
 					return
 				} else {
 					if st == Running || st == StartPending {
-						fmt.Printf("Service \"%s\" is Started.\n", sv.displayName)
-						fmt.Println("can't to stop service.")
+						color.Error.Render("Service \"%s\" is Started.\n", sv.displayName)
+						color.Error.Render("can't to stop service.")
 						return
 					}
 				}
-				fmt.Printf("Service \"%s\" stopped.\n", sv.displayName)
+				color.Info.Render("Service \"%s\" stopped.\n", sv.displayName)
 			}
 		}
 		return
 	}
 
-	fmt.Print("Failed to read args\n")
+	color.Note.Render("Failed to read args\n")
 
 	if err = s.Run(); err != nil {
 		logger.Error(err)
