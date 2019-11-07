e49bc7ff51585d580d0a83c7bcba7f41d18a8df0
谢小军 <346944475@qq.com>
2019-11-07 15:09:52 +0800

    remove go-json-rest

--- tools/json.go
+++ tools/json.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/ant0ine/go-json-rest/rest"
 )
 
 func JsonToForm(r *http.Request) {
@@ -39,15 +37,16 @@ func JsonToForm(r *http.Request) {
 	return
 }
 
-func GetRequestJsonObj(r *rest.Request, v interface{}) error {
+// "github.com/ant0ine/go-json-rest/rest"
+// func GetRequestJsonObj(r *rest.Request, v interface{}) error {
 
-	//添加支持json 操作
-	body, err := ioutil.ReadAll(r.Body)
-	r.Body.Close()
-	json.Unmarshal(body, &v)
-	//-----------------------------end
-	return err
-}
+// 	//添加支持json 操作
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	r.Body.Close()
+// 	json.Unmarshal(body, &v)
+// 	//-----------------------------end
+// 	return err
+// }
 
 func GetJsonStr(obj interface{}) string {
 	b, _ := json.Marshal(obj)
