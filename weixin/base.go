4095fb56197b0f95f97d9f79afa1e075b35179f5
xxjwxc <xie1xiao1jun@qq.com>
2020-03-07 21:16:01 +0800

    new

--- weixin/base.go
+++ weixin/base.go
@@ -6,40 +6,67 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/bitly/go-simplejson"
 	"github.com/xxjwxc/public/mycache"
+	"github.com/xxjwxc/public/myhttp"
 	"github.com/xxjwxc/public/mylog"
-
-	"github.com/silenceper/wechat"
 )
 
 const (
-	_getTicket = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=wx_card&access_token="
-	_getJsurl  = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token="
+	_getTicket    = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=wx_card&access_token="
+	_getJsurl     = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token="
+	_getToken     = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
+	_getSubscribe = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token="
+	_cacheToken   = "wx_access_token"
+	_cacheTicket  = "weixin_card_ticket"
 )
 
 // GetAccessToken 获取微信accesstoken
+
+//获取登录凭证
 func GetAccessToken() (accessToken string, err error) {
-	//先从缓存中获取
-	cache := mycache.OnGetCache("weixin_token")
+	//先从缓存中获取 access_token
+	cache := mycache.OnGetCache(_cacheToken)
 	var tp interface{}
-	tp, b := cache.Value("base")
+	var b bool
+	tp, b = cache.Value(_cacheToken)
 	if b {
-		accessToken = tp.(string)
+		accessToken = *(tp.(*string))
 	} else {
-		wc := wechat.NewWechat(&cfg)
-		accessToken, err = wc.GetAccessToken()
-		//保存缓存
-		cache.Add("base", accessToken, 7000*time.Second)
+		var url = _getToken + wxInfo.AppID + "&secret=" + wxInfo.AppSecret
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return "", err
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		//注入client ip
+		js, err := simplejson.NewJson(body)
+		if err == nil {
+			accessToken, _ = js.Get("access_token").String()
+			//保存缓存
+			cache.Add(_cacheToken, &accessToken, time.Duration(7000)*time.Second)
+			//------------------end
+		}
+		//----------------------end
 	}
+	//---------------获取 access_token --------end
+
 	return
 }
 
 // GetAPITicket 获取微信卡券ticket
 func GetAPITicket() (ticket string, err error) {
 	//先从缓存中获取
-	cache := mycache.OnGetCache("weixin_card_ticket")
+	cache := mycache.OnGetCache(_cacheTicket)
 	var tp interface{}
-	tp, b := cache.Value("base")
+	tp, b := cache.Value(_cacheTicket)
 	if b {
 		ticket = tp.(string)
 	} else {
@@ -68,7 +95,7 @@ func GetAPITicket() (ticket string, err error) {
 		json.Unmarshal(body, &result)
 		ticket = result.Ticket
 		//保存缓存
-		cache.Add("base", ticket, 7000*time.Second)
+		cache.Add(_cacheTicket, ticket, 7000*time.Second)
 	}
 	return
 }
@@ -110,4 +137,20 @@ func GetJsTicket() (ticket string, err error) {
 		cache.Add("base", ticket, 7000*time.Second)
 	}
 	return
+}
+
+// 发送订阅消息
+func SendTemplateMsg(msg TempMsg) bool {
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		mylog.Error(err)
+		return false
+	}
+
+	bo, _ := json.Marshal(msg)
+	resb := myhttp.OnPostJSON(_getSubscribe+accessToken, string(bo))
+
+	var res ResTempMsg
+	json.Unmarshal(resb, &res)
+	return res.Errcode == 0
 }
